5d382ab511cc9d31b9e2742485946c0ff3dc8a16
ska42 <skalyxe00@gmail.com>
2022-03-04 12:14:12 +0100

    hard but it compile

--- tinky/srcs/tinky.c
+++ tinky/srcs/tinky.c
@@ -1,7 +1,16 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 #include <stdio.h>
 
 int			main(void)
 {
 	printf("Hello world !\n");
+	SC_HANDLE svc = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
+	if (!svc)
+	{
+		printf("ko\n");
+		return (1);
+	}
+	printf("youhou\n");
 	return (0);
 }
