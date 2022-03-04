74dae6d16ef74d2dde1d6cac8a9ef4bda1d575b5
ska42 <skalyxe00@gmail.com>
2022-03-04 15:50:37 +0100

    struct program

--- tinky/includes/tinky.h
+++ tinky/includes/tinky.h
@@ -1,8 +1,10 @@
 #ifndef TINKY_H
 # define TINKY_H
 
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#include <stdio.h>
+# define WIN32_LEAN_AND_MEAN
+# include <windows.h>
+# include <stdio.h>
+
+# define SERVICE_NAME "tinky"
 
 #endif
--- tinky/srcs/tinky.c
+++ tinky/srcs/tinky.c
@@ -1,30 +1,56 @@
 #include "tinky.h"
 
-int			main(int ac, char **av)
+int			delete(SC_HANDLE scm)
 {
-	(void)ac;
-	printf("Hello world !\n");
-	SC_HANDLE scm, scs;
-	scm = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
-	if (!scm)
-	{
-		printf("%s: %s() failed, error: %ld\n", av[0], "OpenSCManager", GetLastError());
-		return (1);
-	}
-	scs = OpenService(scm, "tinky", DELETE);
+	SC_HANDLE scs = OpenService(scm, SERVICE_NAME, DELETE);
 	if (scs)
 	{
 		if (!DeleteService(scs))
-			printf("%s: %s() failed, error: %ld\n", av[0], "DeleteService", GetLastError());
+			printf("%s() failed, error: %ld\n", "DeleteService", GetLastError());
 		if (!CloseServiceHandle(scs))
-			printf("%s: %s() failed, error: %ld\n", av[0], "CloseServiceHandle", GetLastError());
+			printf("%s() failed, error: %ld\n", "CloseServiceHandle", GetLastError());
 	}
-	scs = CreateService(scm, "tinky", "tinky", SERVICE_ALL_ACCESS, SERVICE_WIN32_OWN_PROCESS, SERVICE_DEMAND_START, SERVICE_ERROR_NORMAL, av[0], NULL, NULL, NULL, NULL, NULL);
+	return (0);
+}
+
+int			install(SC_HANDLE scm, char *bin_path)
+{
+	delete(scm);
+	SC_HANDLE scs = CreateService(scm, SERVICE_NAME, SERVICE_NAME, SERVICE_ALL_ACCESS, SERVICE_WIN32_OWN_PROCESS, SERVICE_DEMAND_START, SERVICE_ERROR_NORMAL, bin_path, NULL, NULL, NULL, NULL, NULL);
 	if (!scs)
 	{
-		printf("%s: %s() failed, error: %ld\n", av[0], "CreateService", GetLastError());
+		printf("%s() failed, error: %ld\n", "CreateService", GetLastError());
 		return (1);
 	}
-	printf("service !\n");
+	return (0);
+}
+
+int			main(int ac, char **av)
+{
+	if (ac != 2 || (strcmp(av[1], "install") && \
+					strcmp(av[1], "start") && \
+					strcmp(av[1], "stop") && \
+					strcmp(av[1], "delete")))
+	{
+		printf("Usage: %s {install,start,stop,delete}\n", strrchr(av[0], '\\') + 1);
+		return (1);
+	}
+	SC_HANDLE scm;
+	scm = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
+	if (!scm)
+	{
+		printf("%s() failed, error: %ld\n", "OpenSCManager", GetLastError());
+		return (1);
+	}
+	if (!strcmp(av[1], "install"))
+		return (install(scm, av[0]));
+	else if (!strcmp(av[1], "start"))
+	{
+	}
+	else if (!strcmp(av[1], "stop"))
+	{
+	}
+	else if (!strcmp(av[1], "delete"))
+		return (delete(scm));
 	return (0);
 }
